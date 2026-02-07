035c0759ec2348cf51cdc0a7381bc0497962e9cb
Raiza-Hub <wisdomadebola62@gmail.com>
2026-02-07 10:16:23 +0100

    feat: Initialize Next.js drivers application and configure shared UI package.

---
+++ apps/drivers/app/page.tsx
@@ -0,0 +1,11 @@
+import Image, { type ImageProps } from "next/image";
+import { Button, buttonVariants } from "@repo/ui/components/button";
+
+
+export default function Home() {
+  return (
+    <div className="bg-red-200">
+     <Button>hey</Button>
+    </div>
+  );
+}
