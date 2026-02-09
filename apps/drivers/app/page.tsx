fef8e9b6a2c7cae64db60d41063047d080052f65
Raiza-Hub <wisdomadebola62@gmail.com>
2026-02-09 21:53:53 +0100

    feat: create a dedicated sign-up page and relocate the onboarding form from the root page.

--- apps/drivers/app/page.tsx
+++ apps/drivers/app/page.tsx
@@ -1,14 +1,9 @@
 import Image, { type ImageProps } from "next/image";
-import { Button, buttonVariants } from "@repo/ui/components/button";
-import OnboardingForm from "./components/onboarding-form/onboardingForm";
-
 
 export default function Home() {
   return (
     <div className="">
-      <div className="mx-auto max-w-7xl">
-        <OnboardingForm />
-      </div>
+      hey
     </div>
   );
 }
